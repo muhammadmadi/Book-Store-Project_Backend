29ff3c0d9fa8d2a4e98971f06827a114fbb21887
muhammadmadi <muhammadmadi1990@gmail.com>
2023-09-20 20:36:37 +0300

    changing ports

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-export const PORT = process.env.PORT || 5555;
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+
+export const PORT = process.env.PORT ||5555 ;
 
 //export const mongoDBURL = 'mongodb+srv://Admin:Admin@book-store-mern.sojedtm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp' ;
 export const mongoDBURL =   'mongodb+srv://Admin:Admin@book-store-mern.sojedtm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp' ;
