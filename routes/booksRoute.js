3ef662139a44322add4f228d29d7d4eb75eacbf8
muhammadmadi <muhammadmadi1990@gmail.com>
2023-09-20 22:41:29 +0300

    delete model

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -1,5 +1,28 @@
 import express from 'express';
-import { Book } from '../models/bookModel.js';
+import mongoose from 'mongoose';
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
 
 const router = express.Router();
 
