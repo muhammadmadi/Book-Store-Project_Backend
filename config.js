74b9687b20b894f2799a52e68eb2efd80b4a1ff9
muhammadmadi <muhammadmadi1990@gmail.com>
2023-09-20 15:11:45 +0300

    ready to deploy api and dv

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT || 5555;
+
+//export const mongoDBURL = 'mongodb+srv://Admin:Admin@book-store-mern.sojedtm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp' ;
+export const mongoDBURL =   'mongodb+srv://Admin:Admin@book-store-mern.sojedtm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp' ;
